eccb6a5d42473d293a030d68465f8bb3e6701e75
enisn <iletisim@enisnecipoglu.com>
2021-11-24 09:45:09 +0300

    Create BloggingDemoDataSeeder.cs

---
+++ modules/blogging/app/Volo.BloggingTestApp/BloggingDemoDataSeeder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Blogging.Blogs;
+using Volo.Blogging.Posts;
+
+namespace Volo.BloggingTestApp
+{
+    public class BloggingDemoDataSeeder : IDataSeedContributor, ITransientDependency
+    {
+        protected IBlogRepository blogRepository;
+        protected IPostRepository postRepository;
+
+        public BloggingDemoDataSeeder(IBlogRepository blogRepository, IPostRepository postRepository)
+        {
+            this.blogRepository = blogRepository;
+            this.postRepository = postRepository;
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            var blog = await blogRepository.InsertAsync(
+                new Blog(
+                    Guid.NewGuid(),
+                    "default",
+                    "default")
+                );
+
+            var post = await postRepository.InsertAsync(
+                 new Post(
+                     Guid.NewGuid(),
+                     blog.Id,
+                     "Hello World!",
+                     "https://dummyimage.com/600x400/000/fff",
+                     "hello-world"
+                     ));
+
+            post.Content = "#Hello World \nThis is a sample post.";
+        }
+    }
+}
